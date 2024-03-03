5c916746b51a74de1bf9fac8ea893ea15d7a8fd7
Irving Meza <irvingmeza95@gmail.com>
2024-03-02 19:07:29 -0600

    commit de actualizacion.

--- src/main/java/com/TeamCode/serviciousuarios/models/ResponseMessage.java
+++ src/main/java/com/TeamCode/serviciousuarios/models/ResponseMessage.java
@@ -5,16 +5,4 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class ResponseMessage {
     public String message;
-
-    public ResponseMessage(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
